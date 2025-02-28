3769400bccfb4e927db2d313b9015ff3b43261fa
Risan <borhansiddque19@gmail.com>
2025-02-28 20:18:21 +0600

    Refactor color selection buttons and enhance click functionality to update product image

--- conceptual sessions two/script/script.js
+++ conceptual sessions two/script/script.js
@@ -1,14 +1,20 @@
 const ringButtons = document.querySelectorAll(".ring-button");
 
-for(let i = 0; i < ringButtons.length; i++) {
+for (let i = 0; i < ringButtons.length; i++) {
     const ringBtn = ringButtons[i];
     ringBtn.addEventListener('click', function (event) {
-        for(let j = 0; j < ringButtons.length; j++) {
+        const name = event.target.id;
+        const colorName = name.split('-');
+        
+        for (let j = 0; j < ringButtons.length; j++) {
             ringButtons[j].classList.remove('border-purple-700');
             ringButtons[j].classList.add('border-gray-300');
         }
 
         event.target.classList.add('border-purple-700');
+
+        const productImage = document.getElementById('product-image');
+        productImage.src = `./images/${colorName[0]}.png`;
     });
-    
+
 }
